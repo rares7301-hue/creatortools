9430511073791e2d06b396f90cd87ac3b14ce9ea
rares7301-hue <rares7301@gmail.com>
2026-09-05 02:11:45 +0300

    Implement service worker for caching files

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "creatortools-v1";
+
+const FILES_TO_CACHE = [
+  "/creatortools/",
+  "/creatortools/index.html",
+  "/creatortools/style.css",
+  "/creatortools/script.js",
+  "/creatortools/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
